3abff77288c8023b6d0794db70626bd220493896
thecodemaster12 <thecodemaster26@gmail.com>
2026-07-13 13:25:02 +0600

    react router dom

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,22 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { createBrowserRouter, Router, RouterProvider } from 'react-router-dom'
+import MathPage from './pages/MathPage.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+  },
+  {
+    path: '/math',
+    element: <MathPage />,
+  },
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
