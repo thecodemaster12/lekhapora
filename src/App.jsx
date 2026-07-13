86d3bb6e2d000d4c855ecea19a89e58f9820fb3e
thecodemaster12 <thecodemaster26@gmail.com>
2026-07-13 12:49:28 +0600

    Header and Hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,20 @@
+import HeroSection from "./components/HeroSection"
+
 const App = () => {
   return (
-    <>
-      <h1 className="text-center">HI</h1>
-    </>
+    <div className="min-h-screen flex flex-col">
+      <header className="bg-natural flex justify-between items-center h-[60px] shadow-md px-4">
+        <h1 className="logo text-primary text-lg font-bold">LekhaPora</h1>
+        <div className="">
+          <button>Sing Up</button>
+          <button>Sing In</button>
+        </div>
+      </header>
+      <main className="flex-1">
+        <HeroSection />
+      </main>
+      <footer>Footer</footer>
+    </div>
   )
 }
 
